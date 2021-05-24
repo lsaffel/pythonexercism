db6c8b6526773e3598fe80b301a1fb9cfac410f0
Lisa <llvvss@icloud.com>
2021-05-24 17:33:53 -0600

    added a few statements to test the function

--- twofer/two_fer.py
+++ twofer/two_fer.py
@@ -1,11 +1,8 @@
-def two_fer(name="no name given"):
-    if name == "no name given":
-        return "One for you, one for me."
-    else:
-        return "One for " + name + ", one for me."
+def two_fer(name=""):
+    return "One for you, one for me." if not name else f"One for {name}, one for me."
 
 
-print(two_fer("Irving"))
-print(two_fer())
-print(two_fer("Frederick"))
-print(two_fer("Sam"))
+# print(two_fer("Irving"))
+# print(two_fer())
+# print(two_fer("Frederick"))
+# print(two_fer("Sam"))
