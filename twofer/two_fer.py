c2ddd153bf6291381bff05bf3bab951f427a849c
Lisa <llvvss@icloud.com>
2021-05-24 15:40:32 -0600

    Update two_fer.py

--- twofer/two_fer.py
+++ twofer/two_fer.py
@@ -3,3 +3,9 @@ def two_fer(name="no name given"):
         return "One for you, one for me."
     else:
         return "One for " + name + ", one for me."
+
+
+print(two_fer("Irving"))
+print(two_fer())
+print(two_fer("Frederick"))
+print(two_fer("Sam"))
