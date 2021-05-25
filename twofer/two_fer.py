cebdfaa718bf0bd2f4b717f2adb8e4781448f12b
Lisa <llvvss@icloud.com>
2021-05-25 13:24:24 -0600

    Update two_fer.py

--- twofer/two_fer.py
+++ twofer/two_fer.py
@@ -1,8 +1,6 @@
-def two_fer(name=""):
-    return "One for you, one for me." if not name else f"One for {name}, one for me."
+def two_fer(name="you"):
+    return f"One for {name}, one for me."
 
 
-# print(two_fer("Irving"))
-# print(two_fer())
-# print(two_fer("Frederick"))
-# print(two_fer("Sam"))
+print(two_fer("Irving"))
+print(two_fer())
