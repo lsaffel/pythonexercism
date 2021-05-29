a78536a61b7a2afeb37d907670ff1c25b84b9bcd
Lisa Saffel <58794458+lsaffel@users.noreply.github.com>
2021-05-29 10:07:53 -0600

    Update raindrops.py

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,13 @@
 def convert(number):
-    pass
+    result = ""
+    if number % 3 == 0:
+        result = result + "Pling"
+    if number % 5 == 0:
+        result = result + "Plang"
+    if number % 7 == 0:
+        result = result + "Plong"
+
+    if result == "":
+        return number
+    else:
+        return result
