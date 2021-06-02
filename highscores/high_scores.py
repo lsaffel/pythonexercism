7319c4e5bfd0e630cc391338bd6565f2548c1c0d
Lisa Saffel <58794458+lsaffel@users.noreply.github.com>
2021-06-01 20:29:40 -0600

    Update high_scores.py

--- highscores/high_scores.py
+++ highscores/high_scores.py
@@ -1,10 +1,23 @@
 def latest(scores):
-    pass
+    # return the last score in the list, which would be the most recent
+    return scores[-1]
 
 
 def personal_best(scores):
-    pass
+    scores.sort()
+
+    # return the last score in the sorted list, which would be the highest
+    return scores[-1]
 
 
 def personal_top_three(scores):
-    pass
+    scores.sort()
+    num_of_scores = len(scores)
+    scores_list = []
+
+    # append only the last 3 scores to the list... up to 3 maximum
+    for i in range(1, num_of_scores+1):
+        if i < 4:
+            scores_list.append(scores[-i])
+
+    return scores_list
